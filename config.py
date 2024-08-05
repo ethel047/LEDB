01a7f63478304f19bb32547a83175c324a8d080e
ethel <bapmylove0127@gmail.com>
2024-08-05 16:54:34 +0800

    Remove config.py file

--- config.py
+++
@@ -1,7 +0,0 @@
-API_URL = "http://140.115.8.60:3000/api/v1/prediction/55aa8a7b-f98f-4568-a467-7a989a7a08a7"
-Headers = {"Authorization": "Bearer DjBzgumyNoRUsEy4djd4d0WeKAZPo2wEfZ7soBdPkz0=","Guard-Token" :"leda-flowise" }
-ACCESS_TOKEN = 'maPb15b779ZFypQ+zlGwWgzJDTz9dCE/ATi1FBaLdTtJWr0VLr3wpw+VuuCt+1cIqQ/EEBzv+Fz2o8ujvDbADqhUHyjDZc6FHrX7ZFL++bvmJmg5hTcjKwQaH7mNIyTZ75d5tc7p0VxlWwL9Hc/68gdB04t89/1O/w1cDnyilFU='
-WEBHOOK_SECRET = 'c90f69b490f6a8d220d5207d7a92b0d8'
-SERVICE_FILE = './key.json'
-# SERVICE_FILE = 'C:\\Users\\user\\Desktop\\113summerV\\LEDB_0801\\LEDB_0731\\key.json'
-SPREADSHEET_ID = '1wFM0u_7YxVSURxSEq0BcIc9cNVYgRCQJhq619u93VBk'
