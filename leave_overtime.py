9fd5b3620f9ef5970145a469857798b79f9251ab
Yanxxx7979 <i.x.yan0097@gmail.com>
2024-08-07 15:35:48 +0800

    feat: 沒有~號版本

--- leave_overtime.py
+++ leave_overtime.py
@@ -27,40 +27,40 @@ def leave_talking(leave_requests,line_bot_api, user_id, message, user_name):
 
             if not re.match(pattern, leave_date):
                 print('no')
-                if "～" in leave_date:
-                    print("到")
-                    start_date_str, end_date_str = leave_date.split('～')
-                    # 验证并格式化日期
-                    start_date = datetime.strptime(start_date_str.strip(), "%m-%d")
-                    end_date = datetime.strptime(end_date_str.strip(), "%m-%d")
-                    if not re.match(pattern, start_date_str) or not re.match(pattern, end_date_str):
-                        state = 1
-                        print('wrong1')
-                        leave_date=None
-                        leave_requests.pop('請假日期',None)
-                        return "輸入的日期格式不正確，請使用 MM/DD~MM/DD 格式"
-                    else:
-                        state = 2
-                        print(start_date, end_date)
-                        file_leave = read_excel_to_dict(f"/mnt/data/{leave_date}_請假單.xlsx")
-                        print(file_leave)
-                        if user_name not in file_leave:
-                            state = 3
-                            # print(leave_requests)
-                            return f"好的{user_name}，請問你的請假理由"
-                        else:
-                            print('already')
-                            leave_requests.clear()
-                            file_leave.clear()
-                            state=0
-                            return f"你已經請過{leave_date}的假囉"
-                else:
-                    print(leave_date)
-                    state = 1
-                    print('wrong2')
-                    leave_date=None
-                    leave_requests.pop('請假日期',None)
-                    return "輸入的日期格式不正確，請使用 MM/DD 格式"
+                # if "～" in leave_date:
+                #     print("到")
+                #     start_date_str, end_date_str = leave_date.split('～')
+                #     # 验证并格式化日期
+                #     start_date = datetime.strptime(start_date_str.strip(), "%m-%d")
+                #     end_date = datetime.strptime(end_date_str.strip(), "%m-%d")
+                #     if not re.match(pattern, start_date_str) or not re.match(pattern, end_date_str):
+                #         state = 1
+                #         print('wrong1')
+                #         leave_date=None
+                #         leave_requests.pop('請假日期',None)
+                #         return "輸入的日期格式不正確，請使用 MM/DD~MM/DD 格式"
+                #     else:
+                #         state = 2
+                #         print(start_date, end_date)
+                #         file_leave = read_excel_to_dict(f"/mnt/data/{leave_date}_請假單.xlsx")
+                #         print(file_leave)
+                #         if user_name not in file_leave:
+                #             state = 3
+                #             # print(leave_requests)
+                #             return f"好的{user_name}，請問你的請假理由"
+                #         else:
+                #             print('already')
+                #             leave_requests.clear()
+                #             file_leave.clear()
+                #             state=0
+                #             return f"你已經請過{leave_date}的假囉"
+                # else:
+                print(leave_date)
+                state = 1
+                print('wrong2')
+                leave_date=None
+                leave_requests.pop('請假日期',None)
+                return "輸入的日期格式不正確，請使用 MM/DD 格式"
             else:
                 state = 2
                 print(leave_date)
